a445c4f043db795b5e7b84297d4f398f9cc78753
Devin Li <devinli@usc.edu>
2022-12-04 01:54:48 +0000

    final commit with wordle, schedwork, counting, and probability

--- schedwork.cpp
+++ schedwork.cpp
@@ -19,7 +19,29 @@ static const Worker_T INVALID_ID = (unsigned int)-1;
 
 
 // Add prototypes for any helper functions here
+bool scheduleHelper(
+    size_t dailyNeed,
+    size_t maxShifts,
+    int days, 
+    int workers, 
+    int dIndex, 
+    int wIndex, 
+    const AvailabilityMatrix& avail, 
+    DailySchedule& sched, 
+    vector<int> mTracker, 
+    vector<int> nTracker);
 
+bool isValid(
+    size_t dailyNeed, 
+    size_t maxShifts, 
+    vector<int> mTracker, 
+    vector<int> nTracker);
+
+bool finalisValid(
+    size_t dailyNeed, 
+    size_t maxShifts, 
+    vector<int> mTracker, 
+    vector<int> nTracker);
 
 // Add your implementation of schedule() and other helper functions here
 
@@ -35,9 +57,85 @@ bool schedule(
     }
     sched.clear();
     // Add your code below
+    sched.resize(avail.size());
+    vector<int> mTracker(int(avail[0].size()), 0);
+    vector<int> nTracker(int(avail.size()), 0);
+    return scheduleHelper(dailyNeed, maxShifts, nTracker.size(), mTracker.size(), 0, 0, avail, sched, mTracker, nTracker);        
+}
 
+bool scheduleHelper(
+    size_t dailyNeed,
+    size_t maxShifts,
+    int days, 
+    int workers, 
+    int dIndex, 
+    int wIndex, 
+    const AvailabilityMatrix& avail, 
+    DailySchedule& sched, 
+    vector<int> mTracker, 
+    vector<int> nTracker)
+{
+    if(dIndex == days-1 && wIndex == workers){
+        return finalisValid(dailyNeed, maxShifts, mTracker, nTracker);
+    }
 
+    if(wIndex == workers){
+        dIndex++;
+        wIndex = 0;
+    }
 
+    if(avail[dIndex][wIndex] == 0){
+        return scheduleHelper(dailyNeed, maxShifts, days, workers, dIndex, wIndex+1, avail, sched, mTracker, nTracker);
+    }
+
+    sched[dIndex].push_back(wIndex);
+    nTracker[dIndex]++;
+    mTracker[wIndex]++;
+    if(isValid(dailyNeed, maxShifts, mTracker, nTracker)){
+        if(scheduleHelper(dailyNeed, maxShifts, days, workers, dIndex, wIndex+1, avail, sched, mTracker, nTracker)){
+            return true;
+        }
+    }
+    sched[dIndex].pop_back();
+    nTracker[dIndex]--;
+    mTracker[wIndex]--;
+    return scheduleHelper(dailyNeed, maxShifts, days, workers, dIndex, wIndex+1, avail, sched, mTracker, nTracker);
 
 }
 
+bool isValid(
+    size_t dailyNeed, 
+    size_t maxShifts,  
+    vector<int> mTracker, 
+    vector<int> nTracker)
+{
+    for(int i = 0; i < mTracker.size(); i++){
+        if(mTracker[i] > maxShifts){
+            return false;
+        }
+    }
+    for(int i = 0; i < nTracker.size(); i++){
+        if(nTracker[i] > dailyNeed){
+            return false;
+        }
+    }
+    return true;
+}
+
+bool finalisValid(
+    size_t dailyNeed, 
+    size_t maxShifts, 
+    vector<int> mTracker, 
+    vector<int> nTracker){
+    for(int i = 0; i < mTracker.size(); i++){
+        if(mTracker[i] > maxShifts){
+            return false;
+        }
+    }
+    for(int i = 0; i < nTracker.size(); i++){
+        if(nTracker[i] != dailyNeed){
+            return false;
+        }
+    }
+    return true;
+}
--- wordle.cpp
+++ wordle.cpp
@@ -10,6 +10,8 @@ using namespace std;
 
 
 // Add prototypes of helper functions here
+void permute(string in, int inIndex, string req, set<string>* validPermutations);
+void permuteHelper(string in, int beginIndex, int endIndex, set<string>* validPermutations);
 
 
 // Definition of primary wordle function
@@ -19,7 +21,71 @@ std::set<std::string> wordle(
     const std::set<std::string>& dict)
 {
     // Add your code here
+		set<string>* validStrings = new set<string>;
+		set<string> insertions;
+
+		string temp = in;
+
+		int blanks = 0;
+		for(size_t i = 0; i < in.size(); i++){
+			if(in[i] == '-'){
+				blanks++;
+			}
+		}
+		blanks -= floating.size();
+		if(blanks < 0){
+			blanks = 0;
+		}
+
+		string blank(blanks, '-');
+
+		permute(blank, 0, floating, &insertions);
+
+		set<string>::iterator it;
+		for(it = insertions.begin(); it != insertions.end(); it++){
+			int j = 0;
+			for(size_t i = 0; i < temp.size(); i++){
+				if(temp[i] == '-'){
+					temp[i] = (*it)[j];
+					j++;
+				}
+			}
+			if(dict.count(temp) != 0){
+				validStrings->insert(temp);
+			}
+			temp = in;
+		}
+
+		return *validStrings;
 
 }
 
 // Define any helper functions here
+void permute(string in, int inIndex, string req, set<string>* validPermutations){
+	for(int i = 97; i <= 122; i++){
+		if(in[inIndex] == '\0'){
+			permuteHelper(req, 0, req.size()-1, validPermutations);
+			return;
+		}
+		in[inIndex] = char(i);
+		if(in[inIndex+1] != '\0'){
+			permute(in, inIndex+1, req, validPermutations);
+		}
+		else{
+			permuteHelper(in+req, 0, in.size()+req.size()-1, validPermutations);
+		}
+	}
+}
+
+void permuteHelper(string in, int beginIndex, int endIndex, set<string>* validPermutations){
+	if(beginIndex == endIndex){
+		validPermutations->insert(in);
+	}
+	else{
+		for(int i = beginIndex; i <= endIndex; i++){
+			swap(in[i], in[beginIndex]);
+			permuteHelper(in, beginIndex+1, endIndex, validPermutations);
+			swap(in[i], in[beginIndex]);
+		}
+	}
+}
